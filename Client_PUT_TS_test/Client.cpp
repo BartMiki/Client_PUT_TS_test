649edf5af61e801a896ec67ea8fe3e37a0f72d8f
Bartosz Mikulski <bartmiki97@gmail.com>
2017-11-02 21:36:05 +0100

    Client class added

--- Client_PUT_TS_test/Client.cpp
+++ Client_PUT_TS_test/Client.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Client.h"
 
-Client::Client(std::string IP, u_short PORT)
+Client::Client(std::string IP, int PORT)
 {
 	//WinSock2 startup
 	WSAData wsaData;
@@ -11,66 +11,168 @@ Client::Client(std::string IP, u_short PORT)
 	if (WSAStartup(DllVersion, &wsaData) != 0)
 	{
 		MessageBoxA(NULL, "WinSock startup failed", "Error", MB_OK | MB_ICONERROR);
-		exit(1);
+		exit(0);
 	}
-
 	// --- Address setup ---
-
-	inet_pton(AF_INET, IP.c_str(), &(addr.sin_addr)); //Addres
+	SOCKADDR_IN addr; //Addres to bind connection socket to
+	sizeOfAddr = sizeof(SOCKADDR_IN);
+	inet_pton(AF_INET, IP.c_str(), &(addr.sin_addr)); //Addres = local host (this PC)
 	addr.sin_port = htons(PORT); //Port ("htons" means "Host TO Network Short")
 	addr.sin_family = AF_INET; //IPv4 Socket
+
+	clientPtr = this;
 }
 
 bool Client::Connect()
 {
 	// --- Connection socket ---
 	connection = socket(AF_INET, SOCK_STREAM, NULL); //Set connection socket
-	if (connect(connection, (SOCKADDR*)&addr, sizeOfAddr) != 0) //If connecting to server failed
+	int code = connect(connection, (SOCKADDR*)&addr, sizeOfAddr);
+	if (code != 0) //If connecting to server failed
 	{
-		MessageBoxA(NULL, "Failed to connect", "Error", MB_OK | MB_ICONERROR);
+		code = WSAGetLastError();
+		std::string error = "Failed to connect, code: " + std::to_string(code);
+		MessageBoxA(NULL, error.c_str(), "Error", MB_OK | MB_ICONERROR);
 		return false; //Failed to connect to server
 	}
+
 	//Succesful connection to server
+	std::cout << "Connected to server!" << std::endl;
+
+	//Create client thread to handle upcomming messages
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL);
 	return true;
 }
 
 bool Client::CloseConnection()
 {
-	return false;
+	if (closesocket(connection) == SOCKET_ERROR)
+	{
+		if (WSAGetLastError() == WSAENOTSOCK) //If socket error occured
+			return true; //Connection has been closed
+
+		std::string error = "Failed to close Socket, WinSock: "+std::to_string(WSAGetLastError())+".";
+		MessageBoxA(NULL, error.c_str(), "Error", MB_OK | MB_ICONERROR);
+		return false; //Failed to close connection
+	}
+	return true;
 }
 
-bool Client::SendPacket(std::string & str)
+bool Client::SendString(std::string str, Packet packetType)
 {
-
-	return false;
-}
-
-bool Client::ProcessPacket()
-{
-	std::string message;
-	if (!RecvString(message))
+	if (!SendPacketType(packetType)) //If failed to send packet header
 		return false;
-	std::cout << ">" << message << std::endl;
+
+	//str += "\0";
+	u_int64 strSize = str.size();
+	if (!SendInt64(strSize)) //If failed to send size of string
+		return false;
+
+	int check = send(connection, str.c_str(), strSize, NULL); //Send string in strSize bytes
+	if (check == SOCKET_ERROR) //If sending failed
+		return false;
+	return true;
+}
+
+bool Client::SendPacketType(Packet packetType)
+{
+	int check = send(connection, (char*)&packetType, sizeof(Packet), NULL); //Send packetType in 1 byte
+	if (check == SOCKET_ERROR) //If sending failed
+		return false;
+	return true;
+}
+
+bool Client::SendInt64(u_int64 num)
+{
+	int check = send(connection, (char*)&num, sizeof(u_int64), NULL); //Send num in 8 bytes
+	if (check == SOCKET_ERROR) //If sending failed
+		return false;
+	return true;
+}
+
+bool Client::RecvPacketType(Packet & packetType)
+{
+	int check = recv(connection, (char*)&packetType, sizeof(Packet), NULL); //Receive 1 byte (8 bit) packetType
+	if (check == SOCKET_ERROR) //If receiving failed
+		return false;
+	return true;
+}
+
+bool Client::RecvInt64(u_int64 & num)
+{
+	int check = recv(connection, (char*)&num, sizeof(u_int64), NULL); //Receive 8 bytes (64 bits) num
+	if (check == SOCKET_ERROR) //If receiving failed
+		return false;
+	return true;
+}
+
+
+bool Client::RecvString(std::string & str)
+{
+	u_int64 bufferSize;
+	if (!RecvInt64(bufferSize)) //If failed to receive size of upcomming string
+		return false;
+
+	char * buffer = new char[bufferSize + 1];
+	buffer[bufferSize] = '\0'; //adding terminator
+
+	int check = recv(connection, buffer, bufferSize, NULL); //recv string in bufferSize bytes
+	str = buffer;
+	delete[] buffer;
+	if (check == SOCKET_ERROR) //If receiving failed
+		return false;
+	return true;
+}
+
+bool Client::ProcessPacket(Packet packetType)
+{
+	switch (packetType)
+	{
+	case Packet::MsgServer:
+	{
+		std::string message;
+		if (!RecvString(message))
+			return false;
+		std::cout << "# " << message << " #" << std::endl;
+		break;
+	}
+	case Packet::MsgSend:
+	{
+		std::string message;
+		if (!RecvString(message))
+			return false;
+		std::cout << ">" << message << std::endl;
+		Packet packetRecv = Packet::MsgReceived; //Message received properly
+		if (!SendPacketType(packetRecv)) //If there is error in returnin message
+			return false;
+		break;
+	}
+	case Packet::MsgReceived:
+		std::cout << "# Message delivered #" << std::endl;
+		break;
+	default:
+		std::cout << "Unrecognized packet: " << std::hex << (int)packetType << std::endl;
+		break;
+	}
 	return true;
 }
 
 void Client::ClientThread()
 {
-	int bufferSize;
-
+	Packet packetType;
 	while (true)
 	{
-		recv(connection, (char*)&bufferSize, sizeof(int), NULL); //Receive lenght of message
-		char * buffer = new char[bufferSize + 1]; //Buffer fo upcomming message
-		buffer[bufferSize] = '\0';
-		recv(connection, buffer, bufferSize + 1, NULL); //Receive message from sever
-		std::cout << ">" << buffer << std::endl;
-		delete[] buffer;
+		if (!clientPtr->RecvPacketType(packetType)) //If appeard issue with receiving packetType
+			break; //Break infinite loop
+		if (!clientPtr->ProcessPacket(packetType)) //If packet is not properly processed
+			break; //Break infinite loop
+	}
+	std::cout << "# Lost connection to the server. #" << std::endl;
+	if (clientPtr->CloseConnection()) {
+		std::cout << "# Socked to the server was closed succesfuly. #" << std::endl;
+	}
+	else
+	{
+		std::cout << "# Socked was not able to be closed. #" << std::endl;
 	}
 }
-
-bool Client::RecvPacket(std::string & str)
-{
-	return false;
-}
-
--- Client_PUT_TS_test/Client.h
+++ Client_PUT_TS_test/Client.h
@@ -1,4 +1,6 @@
 #pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include "stdafx.h"
 #include <WinSock2.h>
 #include <WS2tcpip.h>
@@ -7,21 +9,49 @@
 
 #pragma comment(lib,"ws2_32.lib") 
 
+enum struct Packet : char {
+	//								  reply  | operation
+	//								---------+----------
+	InviteSend = (char)0x1f,//			none | invite
+	InviteAccepted = (char)0x2f,//	  accept | invite
+	InviteDecline = (char)0x3f,//	 decline | invite
+	MsgSend = (char)0x1e,//				none | sendMsg
+	MsgReceived = (char)0x2e,//		recieved | sendMsg
+	MsgFailure = (char)0x3e,//		  failed | sendMsg
+	MsgServer = (char)0x4e,//			none | sendMsg
+	CloseSession = (char)0x1c,//		none | close
+	CloseSucces = (char)0x2c,//		 success | close
+	CloseFail = (char)0x3c//			fail | close
+};
+
 class Client
 {
 public:
-	Client(std::string IP, u_short PORT);
+	// --- methods ---
+	Client(std::string IP, int PORT);
 	bool Connect();
 	bool CloseConnection();
-	bool SendPacket(std::string & str);
+
+	// --- method send---
+	bool SendString(std::string str, Packet packetType = Packet::MsgSend);
 private:
-
-	bool ProcessPacket();
-	static void ClientThread();
-	bool RecvPacket(std::string & str);
-
+	// --- data fields ---
 	SOCKET connection;
 	SOCKADDR_IN addr; //Addres to bind connection socket to
-	int sizeOfAddr = sizeof(addr);
+	int sizeOfAddr;
+
+	// --- methods send---
+	bool SendPacketType(Packet packetType);
+	bool SendInt64(u_int64 num);
+
+	// --- methods recv ---
+	bool RecvPacketType(Packet & packetType);
+	bool RecvInt64(u_int64 & num);
+	bool RecvString(std::string & str);
+
+	// --- methods others--
+	bool ProcessPacket(Packet packetType);
+	static void ClientThread();
 };
 
+static Client * clientPtr;
